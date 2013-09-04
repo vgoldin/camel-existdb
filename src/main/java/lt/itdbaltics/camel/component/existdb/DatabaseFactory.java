3b2e4d3bfe051cfd98f546444cd4e1205010b1f8
Veniamin Goldin <vgoldin@gmail.com>
2013-09-04 15:21:00 +0200

    Add database shutdown to the destroy method

--- src/main/java/lt/itdbaltics/camel/component/existdb/DatabaseFactory.java
+++ src/main/java/lt/itdbaltics/camel/component/existdb/DatabaseFactory.java
@@ -1,11 +1,13 @@
 package lt.itdbaltics.camel.component.existdb;
 
 import org.exist.xmldb.DatabaseImpl;
+import org.exist.xmldb.DatabaseInstanceManager;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 
 public class DatabaseFactory {
     private static DatabaseImpl database;
+    private static Collection current;
 
     public DatabaseFactory() throws Exception {
         if (database == null) {
@@ -16,17 +18,25 @@ public class DatabaseFactory {
             DatabaseManager.registerDatabase(database);
 
             //FIXME: Externalize database URL
-            DatabaseManager.getCollection("xmldb:exist:///db");
+            current = DatabaseManager.getCollection("xmldb:exist:///db");
         }
     }
 
     public void destroy() {
         if (database != null) {
             try {
+                if (current != null) {
+                    final DatabaseInstanceManager mgr = (DatabaseInstanceManager) current.getService("DatabaseInstanceManager", "1.0");
+
+                    if (mgr != null && mgr.isLocalInstance()) {
+                        mgr.shutdown();
+                    }
+                }
+
                 DatabaseManager.deregisterDatabase(database);
                 database = null;
             } catch (Exception ex) {
-
+                // -- ignore
             }
         }
     }
